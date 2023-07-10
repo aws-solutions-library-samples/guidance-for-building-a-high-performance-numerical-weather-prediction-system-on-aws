861d0b5d6152b0c7421351a0d0d0dddb9729ead9
Daniel Zilberman <42846514+dzilbermanvmw@users.noreply.github.com>
2023-07-10 16:44:56 -0700

    Create load_conus-12-km_wrf_model.sh
    Load CONUS 12-KM model WRF

---
+++ static/load_conus-12-km_wrf_model.sh
@@ -0,0 +1,11 @@
+echo 'Retrieving CONUS 12-km model'
+cd /fsx
+curl -O https://www2.mmm.ucar.edu/wrf/OnLineTutorial/wrf_cloud/wrf_simulation_CONUS12km.tar.gz
+tar -xzf wrf_simulation_CONUS12km.tar.gz
+echo '.. CONUS 12-km model retrieved'
+sleep 10
+echo 'Preparing data for a run - copying relevant files from WRF install'
+cd /fsx/conus_12km/
+WRF_ROOT=$(spack location -i wrf%intel)/test/em_real/
+ln -s $WRF_ROOT* .
+echo '.. data for a WRF run is copied OK'
