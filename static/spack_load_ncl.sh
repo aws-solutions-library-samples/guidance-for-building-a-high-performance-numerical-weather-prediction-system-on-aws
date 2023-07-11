5c695c53d8e203c9cbf6d4290a1cc1e6699c174a
Daniel Zilberman <42846514+dzilbermanvmw@users.noreply.github.com>
2023-07-10 20:42:50 -0700

    Update spack_load_ncl.sh
    moved echo to upper line

--- static/spack_load_ncl.sh
+++ static/spack_load_ncl.sh
@@ -2,5 +2,6 @@ cd /fsx/conus_12km
 echo 'loading Surface Temperature Model... '
 spack load ncl
 spack load intel-oneapi-compilers
+echo '...Surface Temperature Model loaded, visualizing results'
 ncl ncl_scripts/surface.ncl
-echo '...Surface Temperature Model loaded'
+
