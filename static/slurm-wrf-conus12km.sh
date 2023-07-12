314043709424a0cff3d1340771f00b06dd753205
Daniel Zilberman <42846514+dzilbermanvmw@users.noreply.github.com>
2023-07-12 16:21:37 -0700

    Update slurm-wrf-conus12km.sh
    Cleaned up CONUS-12 script

--- static/slurm-wrf-conus12km.sh
+++ static/slurm-wrf-conus12km.sh
@@ -24,12 +24,3 @@ export KMP_AFFINITY=compact
 export I_MPI_DEBUG=4
 
 time mpiexec.hydra -np \$SLURM_NTASKS --ppn \$SLURM_NTASKS_PER_NODE \$wrf_exe
-sleep 10
-echo 'SLURM WRF Conus - 12 km model loaded, submitting a Job...'
-sbatch slurm-wrf-conus12km.sh
-sleep 10
-echo '...WRF Job submitted, checking status'
-squeue
-# Sample output:
-# JOBID PARTITION     NAME     USER ST       TIME  NODES NODELIST(REASON)
-# 5   compute      WRF ec2-user CF       0:19      2 compute-dy-hpc6a-[1-2]
