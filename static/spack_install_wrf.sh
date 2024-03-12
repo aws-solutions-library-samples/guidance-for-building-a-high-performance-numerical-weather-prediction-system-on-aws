5156e47a53638f9d3f7eea01677b6703a3c58965
Daniel Zilberman <42846514+dzilbermanvmw@users.noreply.github.com>
2024-03-11 22:15:02 -0700

    Update spack_install_wrf.sh - removed Intel option since otherwise it was breaking

--- static/spack_install_wrf.sh
+++ static/spack_install_wrf.sh
@@ -1,4 +1,5 @@
 # Install Weather Research and Forecasting (WRF)
 echo 'installing WRF using Intel Compiler and Intel MPI..'
-spack install -j $(nproc) wrf@4.3.3%intel build_type=dm+sm ^intel-oneapi-mpi+external-libfabric
+# spack install -j $(nproc) wrf@4.3.3%intel build_type=dm+sm ^intel-oneapi-mpi+external-libfabric
+spack install wrf
 echo '.. finished installing WRF using Intel Compiler and Intel MPI'
