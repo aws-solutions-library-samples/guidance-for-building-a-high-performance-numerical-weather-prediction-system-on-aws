c943cfcec1b9aa5ba8609c4dbf430336b010d150
Daniel Zilberman <42846514+dzilbermanvmw@users.noreply.github.com>
2024-03-12 09:39:52 -0700

    Update spack_install_wrf.sh - updated WRF version 4.5.2

--- static/spack_install_wrf.sh
+++ static/spack_install_wrf.sh
@@ -1,5 +1,5 @@
 # Install Weather Research and Forecasting (WRF)
 echo 'installing WRF using Intel Compiler and Intel MPI..'
-# spack install -j $(nproc) wrf@4.3.3%intel build_type=dm+sm ^intel-oneapi-mpi+external-libfabric
-spack install wrf
+spack install -j $(nproc) wrf@4.5.2%intel build_type=dm+sm ^intel-oneapi-mpi+external-libfabric
+# For generic version use 'spack install wrf'
 echo '.. finished installing WRF using Intel Compiler and Intel MPI'
