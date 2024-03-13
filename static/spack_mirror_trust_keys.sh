1bddb418e66b8fabef6572c52bf994542b1f3695
Daniel Zilberman <42846514+dzilbermanvmw@users.noreply.github.com>
2024-03-13 12:55:50 -0700

    Update spack_mirror_trust_keys.sh update to vv 0.21.2

--- static/spack_mirror_trust_keys.sh
+++ static/spack_mirror_trust_keys.sh
@@ -1,3 +1,4 @@
 #add the mirror and trust the GPG keys that have signed the packages
-spack mirror add aws-hpc-weather s3://aws-hpc-weather/spack_v0.19.1/
+#spack mirror add aws-hpc-weather s3://aws-hpc-weather/spack_v0.19.1/
+spack mirror add v0.21.2 https://binaries.spack.io/v0.21.2
 spack buildcache keys --install --trust --force
