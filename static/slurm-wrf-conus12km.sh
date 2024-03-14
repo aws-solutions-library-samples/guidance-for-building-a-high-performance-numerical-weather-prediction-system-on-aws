03a42e00db96aa58de4bd1e657de80f6cdde1e86
Daniel Zilberman <42846514+dzilbermanvmw@users.noreply.github.com>
2024-03-14 14:37:31 -0700

    Update slurm-wrf-conus12km.sh - simplified commands for GPU

--- static/slurm-wrf-conus12km.sh
+++ static/slurm-wrf-conus12km.sh
@@ -5,9 +5,8 @@
 #SBATCH --nodes=2
 #SBATCH --ntasks-per-node=48
 #SBATCH --exclusive
+#SBATCH --cpus-per-task=2
 
-export I_MPI_OFI_LIBRARY_INTERNAL=0
-spack load intel-oneapi-mpi intel-oneapi-compilers
 spack load wrf
 module load libfabric-aws
 wrf_exe=$(spack location -i wrf)/run/wrf.exe
@@ -17,10 +16,6 @@ ulimit -a
 
 export OMP_NUM_THREADS=2
 export FI_PROVIDER=efa
-export I_MPI_FABRICS=shm:ofi
-export I_MPI_OFI_PROVIDER=efa
-export I_MPI_PIN_DOMAIN=omp
 export KMP_AFFINITY=compact
-export I_MPI_DEBUG=4
-
-time mpiexec.hydra -np \$SLURM_NTASKS --ppn \$SLURM_NTASKS_PER_NODE \$wrf_exe
+echo " Will run the following command: 'time mpirun -n $SLURM_NTASKS   --bind-to core --map-by slot:pe=${OMP_NUM_THREADS} $wrf_exe'"
+time mpirun  $wrf_exe
